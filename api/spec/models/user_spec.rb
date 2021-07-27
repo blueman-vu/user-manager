39fbb3d9811a4f4d35d11aed9558c08aa521b082
blueman <blueman.vu@gmail.com>
2021-07-27 18:18:06 +0700

    fix: rspec user model

--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -17,4 +17,33 @@ RSpec.describe User, type: :model do
   }
 
   it { should validate_inclusion_of(:role).in_array(%w[user admin]) }
+  
+  describe 'list_user' do
+    let!(:user1) {create(:user)}
+    let!(:user2) {create(:user, email: 'alice@gmail.com')}
+    let!(:user3) {create(:user, username: 'alice july')}
+    let!(:user4) {create(:user, role: 'admin')}
+    let!(:user5) {create(:user, username: 'alice lucy', is_block: true)}
+    let!(:user6) {create(:user, is_block: true)}
+
+    context 'filter with admin' do
+      it 'search all' do
+        expect(User.list_user('admin', '').count).to eq(6)
+      end
+
+      it 'search by alice' do 
+        expect(User.list_user('admin', 'alice').count).to eq(3)
+      end
+    end
+
+    context 'filter with user' do
+      it 'user search all' do
+        expect(User.list_user('user', '').count).to eq(3)
+      end
+
+      it 'search alice' do
+        expect(User.list_user('user', 'alice').count).to eq(2)
+      end
+    end
+  end
 end
