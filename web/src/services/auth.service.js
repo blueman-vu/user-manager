27d0cce5d4451b58ea787cbba40bc379c93efacc
blueman <blueman.vu@gmail.com>
2021-07-27 10:38:38 +0700

    feat: service for authorize & get detail user

---
+++ web/src/services/auth.service.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+ronst API_URL = process.env.BACKEND_API_URL || "http://backend.localhost/";
+
+class AuthService {
+  login(username, password) {
+    return axios
+      .post(API_URL + "auth/login", {
+        username,
+        password,
+      })
+      .then((response) => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+  }
+
+  register(username, email, password) {
+    return axios.post(API_URL + "signup", {
+      username,
+      email,
+      password,
+    });
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  }
+}
+
+export default new AuthService();
